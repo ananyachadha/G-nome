557794f7e9e117d2287237772a73688b3a79bec9
Kevin <kevin.zhang10@gmail.com>
2017-07-25 13:37:05 -0400

    updated migrations

--- app/App.js
+++ app/App.js
@@ -3,6 +3,10 @@ import { connect } from 'react-redux'
 import { setUser } from './actions/userAction'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 import { AppDrawer, Toolbar } from './components/index'
+import Web3 from 'web3'
+import BN from 'bn.js';
+import contract from 'truffle-contract'
+import MetaCoin from '../build/contracts/MetaCoin.json'
 
 injectTapEventPlugin();
 
@@ -10,18 +14,46 @@ class App extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {}
   }
 
   componentDidMount() {
     this.props.dispatch(setUser('111111', 'Testing dispatch'))
   }
 
+  getWeb3() {
+    let web3 = window.web3
+
+    if (typeof web3 !== 'undefined') {
+      web3 = new Web3(web3.currentProvider)
+      console.log('Injected web3 detected.');
+      this.state.web3 = web3
+    } else {
+      var provider = new Web3.providers.HttpProvider('http://localhost:8545')
+      web3 = new Web3(provider)
+      console.log('No web3 instance injected, using Local web3.');
+      this.state.web3 = web3
+    }
+  }
+
   render() {
+      let metacoin = contract(MetaCoin)
+      this.getWeb3()
+      metacoin.setProvider(this.state.web3.currentProvider)
+      let metacoinInstance
+      this.state.web3.eth.getAccounts((error, accounts) => {
+        metacoin.deployed().then((instance) => {
+          metacoinInstance = instance
+          return metacoinInstance.getBalance.call("0x0Aa6b15E6dC54155f79BBb536D8C0c9195F1F27D", {from: accounts[0]})
+        }).then((result) => {
+          this.setState({balance: result.c[0]});
+        })
+      })
       return (
         <div>
           <Toolbar />
           <AppDrawer />
-          {this.props.main.name}
+          <span>Balance: {this.state.balance}</span>
         </div>
       )
   }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,12 @@
 var ConvertLib = artifacts.require("./ConvertLib.sol");
 var MetaCoin = artifacts.require("./MetaCoin.sol");
 
-module.exports = function(deployer) {
+module.exports = function(deployer, network, accounts) {
   deployer.deploy(ConvertLib);
   deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  deployer.then(function() {
+    return MetaCoin.deployed()
+  }).then(function(instance) {
+    return instance.sendCoin("0x0Aa6b15E6dC54155f79BBb536D8C0c9195F1F27D", 101, {from:accounts[0]})
+  })
 };
